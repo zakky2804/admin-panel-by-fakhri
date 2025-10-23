d17a259cc5b310f7a63a8cdeefb73feae4430577
zakky <fakhrimuzakki06@gmail.com>
2025-10-23 17:04:46 +0700

    perbaiki kesalahan di fetch wrapper

--- components/FetchWrapper.tsx
+++ components/FetchWrapper.tsx
@@ -2,14 +2,18 @@ import { DataAPI } from "@/interfaces/interface";
 import ZustandProvider from "./ZustandProvider";
 
 const FetchWrapper = async ({ children }: React.PropsWithChildren) => {
-  const baseUrl = process.env.BASE_URL;
-  const res = await fetch(`${baseUrl}/data.json`, {
-    cache: "no-store", // atau force-cache, tergantung butuh realtime atau tidak
-    // cache: "force-cache",
-  });
+  try {
+    const baseUrl = process.env.BASE_URL;
+    const res = await fetch(`${baseUrl}/data.json`, {
+      cache: "no-store", // atau force-cache, tergantung butuh realtime atau tidak
+      // cache: "force-cache",
+    });
 
-  const data: DataAPI = await res.json();
-  return <ZustandProvider initialdata={data}>{children}</ZustandProvider>;
+    const data: DataAPI = await res.json();
+    return <ZustandProvider initialdata={data}>{children}</ZustandProvider>;
+  } catch (err) {
+    console.error("Fetch JSON failed:", err);
+  }
 };
 
 export default FetchWrapper;
