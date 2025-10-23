0ddc4cfea53106f63709f77428426b060f015ac5
zakky <fakhrimuzakki06@gmail.com>
2025-10-23 16:54:44 +0700

    ubah kesalahan di fetch

--- components/FetchWrapper.tsx
+++ components/FetchWrapper.tsx
@@ -4,8 +4,8 @@ import ZustandProvider from "./ZustandProvider";
 const FetchWrapper = async ({ children }: React.PropsWithChildren) => {
   const baseUrl = process.env.BASE_URL;
   const res = await fetch(`${baseUrl}/data.json`, {
-    // cache: "no-store", // atau force-cache, tergantung butuh realtime atau tidak
-    cache: "force-cache",
+    cache: "no-store", // atau force-cache, tergantung butuh realtime atau tidak
+    // cache: "force-cache",
   });
 
   const data: DataAPI = await res.json();
